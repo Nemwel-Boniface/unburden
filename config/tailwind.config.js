7f0c4c6a72e370cd4e9f1d1e8622beced32c2a34
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-06-18 11:09:22 +0300

    Update add custom stylies to my config files

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      backgroundColor: {
+        'top-nav-light-brown-bg': '#F3EBE6',
+        'dark-brown-bg': '#A57355',
+      },
+      color: {
+        'brown-text': '#A57355',
+      },
     },
   },
   plugins: [
