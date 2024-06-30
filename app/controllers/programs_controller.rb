0e297aca5a1777f3f7d09ef15ef140653c8b01a8
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-06-30 11:39:20 +0300

    Update check and fix all found linter errors

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,5 +1,5 @@
 class ProgramsController < ApplicationController
-  before_action :set_program, only: %i[ show edit update destroy ]
+  before_action :set_program, only: %i[show edit update destroy]
 
   # GET /programs or /programs.json
   def index
@@ -7,8 +7,7 @@ class ProgramsController < ApplicationController
   end
 
   # GET /programs/1 or /programs/1.json
-  def show
-  end
+  def show; end
 
   # GET /programs/new
   def new
@@ -16,8 +15,7 @@ class ProgramsController < ApplicationController
   end
 
   # GET /programs/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /programs or /programs.json
   def create
@@ -25,7 +23,7 @@ class ProgramsController < ApplicationController
 
     respond_to do |format|
       if @program.save
-        format.html { redirect_to program_url(@program), notice: "Program was successfully created." }
+        format.html { redirect_to program_url(@program), notice: 'Program was successfully created.' }
         format.json { render :show, status: :created, location: @program }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +36,7 @@ class ProgramsController < ApplicationController
   def update
     respond_to do |format|
       if @program.update(program_params)
-        format.html { redirect_to program_url(@program), notice: "Program was successfully updated." }
+        format.html { redirect_to program_url(@program), notice: 'Program was successfully updated.' }
         format.json { render :show, status: :ok, location: @program }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +50,20 @@ class ProgramsController < ApplicationController
     @program.destroy!
 
     respond_to do |format|
-      format.html { redirect_to programs_url, notice: "Program was successfully destroyed." }
+      format.html { redirect_to programs_url, notice: 'Program was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_program
-      @program = Program.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def program_params
-      params.fetch(:program, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_program
+    @program = Program.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def program_params
+    params.fetch(:program, {})
+  end
 end
