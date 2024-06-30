c334a518a10c25d605132a680fae74cf3602e9f6
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-06-30 17:39:53 +0300

    Update included:   - Create and style the individual programs right section   - Add custom styles to the Tailwind config file

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -19,7 +19,8 @@ module.exports = {
         'cream': '#F7F2EF',
         'cream-bg': '#FFFFFF',
         'programs-cream': '#F3EBE6',
-        'transparent-bg':'#544F4E'
+        'transparent-bg':'#544F4E',
+        'gray-bg': '#F9F7F7',
       },
       colors: {
         'brown-text': '#A57355',
