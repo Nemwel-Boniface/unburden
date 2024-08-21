8852a8053af7ad7e52e8b8dc9dbc148148b2d07f
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2024-08-21 10:40:52 +0300

    Update included:   - Generated the devise resources for users and psychologist   - Update the routes to include custom routes for user and psychologist

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :programs
+
+  devise_for :users, path: 'users', controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+
+  # Psychologists routes
+  devise_for :psychologists, path: 'psychologists', controllers: {
+    sessions: 'psychologists/sessions',
+    registrations: 'psychologists/registrations',
+    passwords: 'psychologists/passwords'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
